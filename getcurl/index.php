0aa5c193a20af5a62a3a3ee0262cce276b9459db
dopleg <dopleg@example.com>
2021-04-23 05:42:11 -0400

    curl new app

---
+++ getcurl/index.php
@@ -0,0 +1,20 @@
+
+<?php
+// Crear un gestor curl
+$ch = curl_init('http://session-myhello.apps.sharedocp311cns.lab.rdu2.cee.redhat.com');
+
+// Ejecutar
+curl_exec($ch);
+
+// Comprobar si ocurrió un error
+if(!curl_errno($ch))
+{
+ $info = curl_getinfo($ch);
+
+ echo 'Se tardó ' . $info['total_time'] . ' segundos en enviar la petición a ' . $info['url'];
+}
+
+// Cerrar manipulador
+curl_close($ch);
+?>
+
