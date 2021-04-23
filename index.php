18e23b11259e8945395b2ec27e5f5b2348880c63
dopleg <dopleg@example.com>
2021-04-23 04:59:00 -0400

    Get sessioncontent

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 
 <?php
 session_start();
+foreach (getallheaders() as $name => $value) {
+	 echo "$name: $value</br>";
+}
+echo "<h2> This is Server ?</h2></br>";
+	echo "<h2>".$_SESSION["user"]."</h2>";
+	exit;
+
 ?>
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-print_r($_SESSION);
-?>
-
-</body>
-</html>
-
