38d250d8b1be791ed97036d122a6b0319a6ccd85
dopleg <dopleg@example.com>
2021-04-23 05:35:46 -0400

    Add table

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 foreach (getallheaders() as $name => $value) {
 	 echo "$name: $value</br>";
 }
-echo "<h2> This is Server ?</h2></br>";
+echo "<h2> Server ?</h2></br>";
 	echo "<h2>".$_SESSION["user"]."</h2>";
 	exit;
 
